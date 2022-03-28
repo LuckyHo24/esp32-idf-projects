8e03b499c26e01e1f30af7e14dea887d74467ccb
Denver Ho <denver@arevo.com>
2022-03-28 11:32:01 +0700

    Add hello world example.
    Signed-off-by: Denver Ho <denver@arevo.com>

---
+++ hello_world/common/input_iot/input_iot.h
@@ -0,0 +1,18 @@
+#ifndef INPUT_IOT_H
+#define INPUT_IOT_H
+#include <esp_log.h>
+#include <hal/gpio_types.h>
+
+typedef enum {
+    HI_TO_LO = 0,
+    LO_TO_HI,
+    ANY_EDGE
+} interrupt_type_edge_t;
+
+typedef void (*input_callback_t) (int);
+
+void input_io_create(gpio_num_t gpio_num, interrupt_type_edge_t type);
+int input_io_get_level(gpio_num_t gpio_num);
+void input_set_callback(void *cb);
+
+#endif
